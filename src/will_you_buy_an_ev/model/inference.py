4a4ddc6cfee7c25e1675e1cacfc19dce955d8348
Corey Michaud <cor3y.michaud@gmail.com>
2026-09-10 16:21:17 -0400

    feat: inference from saved model

---
+++ src/will_you_buy_an_ev/model/inference.py
@@ -0,0 +1,21 @@
+import joblib
+from will_you_buy_an_ev.config.paths import RAW_DATA_PATH, MODEL_PATH
+import pandas as pd
+
+# Load test
+test = pd.read_csv(RAW_DATA_PATH / "test.csv")
+
+# Load model
+pipe = joblib.load(MODEL_PATH / "lgbmc.joblib")
+
+# The Mode Collapse Spike
+test['is_30k_spike'] = (test['Annual_Income_USD'] == 30000.0).astype('int8')
+
+# The Millionaire Cliff (100% buy rate region)
+test['is_millionaire_cliff'] = (test['Annual_Income_USD'] >= 170537.0).astype('int8')
+
+# The Dead Zone (0% buy rate region)
+test['is_dead_zone'] = ((test['Annual_Income_USD'] >= 38000.0) & (test['Annual_Income_USD'] <= 42000.0)).astype('int8')
+
+probs = pipe.predict_proba(test.drop(columns = ["id"]))
+preds = probs[:,1]
